e0a904d6891bfe2225b0dd65be1979114e6f19d4
taleshmm <taleshmm@hotmail.com>
2023-08-11 17:55:09 -0300

    feat: create service editor and author

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from service.category_service import CategoryService
+from service.editor_service import EditorService
+from service.author_service import AuthorService
 
 category_service = CategoryService()
+editor_service = EditorService()
+author_service = AuthorService()
 
 def principal_menu():
      print('''\n[Menu Principal] Escolha uma das seguintes opções:
   1 - Categorias
+  2 - Editoras
+  3 - Autores(as)
   0 - Sair do programa''')
      selection = input('Digite a opção: ')
      
@@ -13,6 +19,10 @@ def principal_menu():
         return
      if selection == '1':
         category_service.menu()
+     elif selection == '2':
+         editor_service.menu()
+     elif selection == '3':
+         author_service.menu()
      else:
         print('Opção inválida! Por favor, tente novamente!')
      
